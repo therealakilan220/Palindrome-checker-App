6fef1e1a150734b6d125874c3a1e204c7cd19619
therealakilan220 <akilanp5252@gmail.com>
2026-02-26 10:58:06 +0530

    UC2 HARDCORE PALINDROME

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
         public static void main(String[] args) {
-            System.out.println("Welcome to the Palindrome Checker Management System");
-            System.out.println("Version : 1.0");
-            System.out.println("System initialized successfully.");
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Input text: ");
+            String input = scanner.nextLine();
+
+            boolean isPalindrome = true;
+
+            for (int i = 0; i < input.length() / 2; i++) {
+                if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            if (isPalindrome) {
+                System.out.println("Is it a Palindrome? : true");
+            } else {
+                System.out.println("Is it a Palindrome? : false");
+            }
+
+            scanner.close();
+
         }
 
 }
