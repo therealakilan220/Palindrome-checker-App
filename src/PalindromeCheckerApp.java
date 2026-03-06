03fecede67997d7dab3903e163c0a72d521dbf5b
therealakilan220 <akilanp5252@gmail.com>
2026-03-06 11:25:32 +0530

    UC11

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,23 +2,30 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String input = "A man a plan a canal Panama";
+        String input = "racecar";
 
-        // Normalize the string (remove non letters and convert to lowercase)
-        String normalized = input.replaceAll("[^a-zA-Z]", "").toLowerCase();
-
-        boolean isPalindrome = true;
-
-        // Compare symmetric characters
-        for (int i = 0; i < normalized.length() / 2; i++) {
-
-            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
+        boolean result = isPalindrome(input);
 
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome : " + isPalindrome);
+        System.out.println("Is Palindrome : " + result);
+    }
+
+    // Method to check palindrome
+    public static boolean isPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
     }
 }
