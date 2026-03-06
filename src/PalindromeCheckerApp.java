eae558be7dff25e3b962951977fa11ba1d582fc5
therealakilan220 <akilanp5252@gmail.com>
2026-03-06 11:22:26 +0530

    UC10

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,29 +1,24 @@
-public class UseCase9PalindromeCheckerApp {
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String input = "madam";
+        String input = "A man a plan a canal Panama";
 
-        boolean result = checkPalindrome(input, 0, input.length() - 1);
+        // Normalize the string (remove non letters and convert to lowercase)
+        String normalized = input.replaceAll("[^a-zA-Z]", "").toLowerCase();
+
+        boolean isPalindrome = true;
+
+        // Compare symmetric characters
+        for (int i = 0; i < normalized.length() / 2; i++) {
+
+            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
 
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome : " + result);
-    }
-
-    // Recursive method
-    private static boolean checkPalindrome(String str, int start, int end) {
-
-        // Base case
-        if (start >= end) {
-            return true;
-        }
-
-        // If characters don't match
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
-
-        // Recursive call
-        return checkPalindrome(str, start + 1, end - 1);
+        System.out.println("Is Palindrome : " + isPalindrome);
     }
 }
